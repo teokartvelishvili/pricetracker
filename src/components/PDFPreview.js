fb2770a546ec0aeecdb91307f29306faeda73b68
teokartvelishvili <qartvelishviliteona@gmail.com>
2024-11-11 08:42:32 -0800

    ..

--- src/components/PDFPreview.js
+++ src/components/PDFPreview.js
@@ -3,16 +3,36 @@ import html2pdf from 'html2pdf.js';
 
 function PDFPreview({ rows, onClose }) {
   const pdfRef = useRef();
+  const fileName = 'sales_report.pdf';
 
-  const generatePDF = () => {
+  const generatePDF = async (share = false) => {
     const element = pdfRef.current;
     const options = {
       margin: 0.5,
-      filename: 'sales_report.pdf',
+      filename: fileName,
       html2canvas: { scale: 2 },
       jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
     };
-    html2pdf().set(options).from(element).save();
+
+    const pdfInstance = html2pdf().set(options).from(element);
+
+    if (share && navigator.share) {
+      // გენერაცია `output` მეთოდის გამოყენებით, რაც მონაცემებს ტექსტის ფორმატში მიიღებს
+      const pdfBlob = await pdfInstance.output('blob');
+      const pdfFile = new File([pdfBlob], fileName, { type: 'application/pdf' });
+
+      try {
+        await navigator.share({
+          files: [pdfFile],
+          title: 'Sales Report',
+          text: 'Please find the attached PDF report.',
+        });
+      } catch (error) {
+        console.error('Error sharing:', error);
+      }
+    } else {
+      pdfInstance.save();
+    }
   };
 
   const totalSellPrice = rows.reduce((sum, row) => sum + (parseFloat(row.totalPrice) || 0), 0);
@@ -21,7 +41,7 @@ function PDFPreview({ rows, onClose }) {
     <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center">
       <div className="bg-white p-4 rounded max-w-2xl w-full">
         <div ref={pdfRef}>
-          {/* <h2 className="text-lg font-bold mb-4">PDF-ის წინასწარი ნახვა</h2> */}
+          <h2 className="text-lg font-bold mb-4">PDF-ის წინასწარი ნახვა</h2>
           <table className="min-w-full border border-gray-300 mb-4">
             <thead>
               <tr>
@@ -50,7 +70,8 @@ function PDFPreview({ rows, onClose }) {
           </table>
         </div>
         <div className="flex justify-end space-x-4 mt-4">
-          <button onClick={generatePDF} className="bg-blue-500 text-white py-2 px-4 rounded">PDF გადაკეთება</button>
+          <button onClick={() => generatePDF(false)} className="bg-blue-500 text-white py-2 px-4 rounded">PDF გადაკეთება</button>
+          <button onClick={() => generatePDF(true)} className="bg-green-500 text-white py-2 px-4 rounded">PDF გაზიარება</button>
           <button onClick={onClose} className="bg-red-500 text-white py-2 px-4 rounded">დახურვა</button>
         </div>
       </div>
